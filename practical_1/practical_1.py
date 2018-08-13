c2c3eac31037298159aca3abc13d57561a31e671
gahan9 <gahansaraiya@gmail.com>
2018-08-13 08:46:48 +0530

    added references and code

--- practical_1/practical_1.py
+++ practical_1/practical_1.py
@@ -1,6 +1,13 @@
 import json
-
+import csv
+FIELD_NAMES = ["name", "objective", "reference", "pros", "cons", "example"]
 with open("data.json", "r") as f:
     content = json.load(f)
 
-print(content["process_models"][0].values())
+with open("data.csv", "w") as f:
+    csv_writer = csv.DictWriter(f, fieldnames=FIELD_NAMES)
+    csv_writer.writeheader()
+    for row in content.get("process_models", []):
+        if row:
+            csv_writer.writerow(row)
+# print(content["process_models"][0].values())
